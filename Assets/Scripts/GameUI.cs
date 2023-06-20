abab7e2f0bf7da318e758be27327ea4e92de5e50
Sam Radford <smr1292@yahoo.com>
2023-06-20 11:49:58 -0400

    Pause menu added

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField] DirectionalButton[] directionalButtons;
+    [SerializeField] GameObject menuButton;
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject gameOverMenu;
+
+    public void PauseButtonPressed() {
+        foreach(DirectionalButton dbtn in directionalButtons) {
+            dbtn.gameObject.SetActive(false);
+            menuButton.SetActive(false);
+        }
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumePressed() {
+        foreach (DirectionalButton dbtn in directionalButtons) {
+            dbtn.gameObject.SetActive(true);
+            menuButton.SetActive(true);
+        }
+        pauseMenu.SetActive(false);
+
+    }
+
+    public void RestartPressed() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitPressed() {
+        Application.Quit();
+    }
+
+
+}
